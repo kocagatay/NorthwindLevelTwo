f80b42a90af75d817f313aaf3bb3952dde1fadd5
Çağatay Koç <kocagatayy@gmail.com>
2023-07-24 00:25:27 +0300

    Add project files.

---
+++ MvcWebUı/Helpers/CartSessionHelper.cs
@@ -0,0 +1,36 @@
+﻿using Entities.DomainModels;
+using MvcWebUI.Extensions;
+
+namespace MvcWebUI.Helpers
+{
+    public class CartSessionHelper : ICartSessionHelper
+    {
+        IHttpContextAccessor _httpcontextAccessor;
+
+        public CartSessionHelper(IHttpContextAccessor httpcontextAccessor)
+        {
+            _httpcontextAccessor = httpcontextAccessor;
+        }
+
+        public void Clear()
+        {
+            _httpcontextAccessor.HttpContext.Session.Clear();
+        }
+
+        public Cart GetCart(string key)
+        {
+            Cart cartToCheck = _httpcontextAccessor.HttpContext.Session.GetObject<Cart>(key);
+            if (cartToCheck == null)
+            {
+                SetCart(key, new Cart());
+                cartToCheck = _httpcontextAccessor.HttpContext.Session.GetObject<Cart>(key);
+            }
+            return cartToCheck;
+        }
+
+        public void SetCart(string key, Cart cart)
+        {
+            _httpcontextAccessor.HttpContext.Session.SetObject(key, cart);
+        }
+    }
+}
---
+++ MvcWebUı/Models/ShippingDetail.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcWebUI.Models
+{
+    public class ShippingDetail
+    {
+        //[Required(ErrorMessage = "İsim gerekli")] //Data Annotation SOLİD'in S Harfine uyan bir uygulama değildir. Tc kimlik örneği yabancı uyruklular için.
+        //FLUENTVALİDATİON'DA YAZDIK AMA BEN BİLDİĞİM İÇİN YAZMADIM ONU BACKEND'DE HALLEDİYORUZ BUSİNESS'DA HER ZAMANKİ GİBİ
+        //ONA EK OLARAK 2 RULE FOR'UN BİRBİRİNE BAĞLANTILI KURALLANMASINI GÖSTERDİ
+        //ÖRNEK: RuleFor(s=>s.City).NotEmpty().When(s=>s.Age<18); 18 YAŞINDAN KÜÇÜKSE ŞEHİR BİLGİSİNİ O ZAMAN GİRMESİ ZORUNLU GİBİ.
+        public string? FirstName { get; set; }
+        // [Required]
+        public string? LastName { get; set; }
+        //[Required]
+        // [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        //[Required]
+        public string? City { get; set; }
+        //[Required]
+        public string? Address { get; set; }
+        // [Required]
+        // [Range(18, 65)]
+        public int Age { get; set; }
+    }
+}
